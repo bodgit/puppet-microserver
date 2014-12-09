45b76aff5b0a954bc0bcfe4c98b70dbd64a0efcf
Matt Dainty <matt@bodgit-n-scarper.com>
2014-12-09 23:05:37 +0000

    Fill out the specs

--- spec/classes/microserver_spec.rb
+++ spec/classes/microserver_spec.rb
@@ -2,11 +2,37 @@ require 'spec_helper'
 
 describe 'microserver' do
 
+  let(:facts) do
+    {
+      :manufacturer    => 'HP',
+      :productname     => 'MicroServer',
+      :microserver_ilo => false,
+      :microserver_tpm => false
+    }
+  end
+
+  context 'on unsupported hardware' do
+    let(:facts) do
+      super().merge(
+        {
+          :manufacturer => 'Daddies',
+          :productname  => 'MassiveServer'
+        }
+      )
+    end
+
+    it do
+      expect { subject }.to raise_error(/not supported on Daddies MassiveServer hardware/)
+    end
+  end
+
   context 'on unsupported distributions' do
     let(:facts) do
-      {
-        :osfamily => 'Unsupported'
-      }
+      super().merge(
+        {
+          :osfamily => 'Unsupported'
+        }
+      )
     end
 
     it do
@@ -16,9 +42,11 @@ describe 'microserver' do
 
   context 'on RedHat' do
     let(:facts) do
-      {
-        :osfamily => 'RedHat'
-      }
+      super().merge(
+        {
+          :osfamily => 'RedHat'
+        }
+      )
     end
 
     context 'version 6' do
@@ -40,6 +68,38 @@ describe 'microserver' do
         end
 
         it do
+          should contain_class('microserver')
+          should_not contain_class('rngd')
+          should_not contain_class('ipmi')
+          should_not contain_class('watchdog')
+          should_not contain_file('/etc/modprobe.d/sp5100_tco.conf')
+        end
+
+        context 'with ILO present', :compile do
+          let(:facts) do
+            super().merge(
+              {
+                :microserver_ilo => true,
+              }
+            )
+          end
+
+          it do
+          end
+        end
+
+        context 'with TPM present', :compile do
+          let(:facts) do
+            super().merge(
+              {
+                :microserver_tpm => true,
+              }
+            )
+          end
+
+          it do
+            should contain_class('rngd')
+          end
         end
       end
 
@@ -53,6 +113,38 @@ describe 'microserver' do
         end
 
         it do
+          should contain_class('microserver')
+          should_not contain_class('rngd')
+          should_not contain_class('ipmi')
+          should_not contain_class('watchdog')
+          should_not contain_file('/etc/modprobe.d/sp5100_tco.conf')
+        end
+
+        context 'with ILO present', :compile do
+          let(:facts) do
+            super().merge(
+              {
+                :microserver_ilo => true,
+              }
+            )
+          end
+
+          it do
+          end
+        end
+
+        context 'with TPM present', :compile do
+          let(:facts) do
+            super().merge(
+              {
+                :microserver_tpm => true,
+              }
+            )
+          end
+
+          it do
+            should contain_class('rngd')
+          end
         end
       end
     end
@@ -67,6 +159,50 @@ describe 'microserver' do
       end
 
       it do
+        should contain_class('microserver')
+        should_not contain_class('rngd')
+        should_not contain_class('ipmi')
+        should contain_class('watchdog')
+        should contain_file('/etc/modprobe.d/sp5100_tco.conf').with(
+          'ensure' => 'absent'
+        ).that_notifies('Class[watchdog]')
+      end
+
+      context 'with ILO present', :compile do
+        let(:facts) do
+          super().merge(
+            {
+              :microserver_ilo => true,
+            }
+          )
+        end
+
+        it do
+          should contain_class('ipmi').that_notifies('Class[watchdog]')
+          should contain_exec('modprobe -r sp5100_tco').that_requires('File[/etc/modprobe.d/sp5100_tco.conf]').that_notifies('Class[watchdog]')
+          should contain_file('/etc/modprobe.d/ipmi.conf').with(
+            'ensure'  => 'file',
+            'content' => "options ipmi_si type=kcs ports=0xca2\n"
+          ).that_notifies('Class[ipmi]')
+          should contain_file('/etc/modprobe.d/sp5100_tco.conf').with(
+            'ensure'  => 'file',
+            'content' => "blacklist sp5100_tco\n"
+          )
+        end
+      end
+
+      context 'with TPM present', :compile do
+        let(:facts) do
+          super().merge(
+            {
+              :microserver_tpm => true,
+            }
+          )
+        end
+
+        it do
+          should contain_class('rngd')
+        end
       end
     end
   end
