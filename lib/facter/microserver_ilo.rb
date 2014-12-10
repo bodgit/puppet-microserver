77aad3ed9e553fc2ead45925dfa3d127fa474e2a
Matt Dainty <matt@bodgit-n-scarper.com>
2014-12-10 00:03:52 +0000

    Fix ILO fact
    Installing a PCIe card in the x16 slot changes the bus/slot number that the
    remote management card will have so switch to a glob to find it.

--- lib/facter/microserver_ilo.rb
+++ lib/facter/microserver_ilo.rb
@@ -1,3 +1,5 @@
+require 'facter/util/file_read'
+
 # Fact to discern if the remote management card is present
 Facter.add(:microserver_ilo) do
 
@@ -6,16 +8,22 @@ Facter.add(:microserver_ilo) do
   confine :productname => 'ProLiant MicroServer'
 
   setcode do
-    sysfs_root = '/sys/bus/pci/devices/0000:01:00.0/'
+    found = false
 
     # Check for the PCI bridge and the device behind it
-    if Facter::Util::FileRead.read(sysfs_root + 'vendor') == "0x1a03\n" and
-       Facter::Util::FileRead.read(sysfs_root + 'device') == "0x1150\n" and
-       Facter::Util::FileRead.read(sysfs_root + '0000:02:00.0/vendor') == "0x1a03\n" and
-       Facter::Util::FileRead.read(sysfs_root + '0000:02:00.0/device') == "0x2000\n"
-      true
-    else
-      false
+    Dir.glob('/sys/bus/pci/devices/0000:??:00.0').each do |parent|
+      if Facter::Util::FileRead.read(File.join(parent, 'vendor')) == "0x1a03\n" and
+         Facter::Util::FileRead.read(File.join(parent, 'device')) == "0x1150\n"
+
+        Dir.glob(parent + '/0000:??:00.0').each do |child|
+          if Facter::Util::FileRead.read(File.join(child, 'vendor')) == "0x1a03\n" and
+             Facter::Util::FileRead.read(File.join(child, 'device')) == "0x2000\n"
+            found = true
+          end
+        end
+      end
     end
+
+    found
   end
 end
